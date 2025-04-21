61f78e3b8e90fe0acb530bf09e59916d142bc255
huythangdz <hahahnguyenhuythangdz@gmail.com>
2025-04-21 11:43:13 +0700

    toboy.lua

--- Scriptdemo (1).lua
+++ Scriptdemo (1).lua
@@ -34,7 +34,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
      local Tab2o = MakeTab({Name = "Script kaitun"})
      local Tab3o = MakeTab({Name = "Script fram rương"})
 AddButton(Tab2o, {
-  Name = "Redz Hub",
+  Name = "Quartz Kaitun Hub",
  Callback = function()getgenv().Mode = "OneClick"
   getgenv().Setting = {
       ["Team"] = "Pirates", -- Options "Pirates", "Marines"
@@ -57,7 +57,14 @@ AddButton(Tab2o, {
       ["IdleCheck"] = 150, -- every (x) seconds if not moving rejoin
   };
   loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/Loader.lua"))() end })    AddButton (Tab1o, {Name = "banana hub",Callback = function()loadstring(game:HttpGet("https://raw.githubusercontent.com/kimprobloxdz/Banana-Free/refs/heads/main/Protected_5609200582002947.lua.txt"))() end })
-	  
+	AddButton (Tab1o, {Name = "banana hub",Callback = function()loadstring(game:HttpGet("https://raw.githubusercontent.com/kimprobloxdz/Banana-Free/refs/heads/main/Protected_5609200582002947.lua.txt"))() end })
+  AddButton(Tab1o, {
+    Name = "Maru Hub",
+   Callback = function()getgenv().Team = "Marines"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/KimP/refs/heads/main/MaruHub"))() end }) AddButton(Tab1o,{Name ="Lion Hub ",Callback = function()repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+    getgenv().team = "Pirates" -- Marines
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e0c7fcf6c077fc23475cf4ce4db58e42.lua"))() }) AddButton(Tab1o, {
+      Name = "Blue X ",Callback = function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/Main.lua"))()  
     AddButton(Tab1o, {
      Name = "Redz Hub",
     Callback = function()
@@ -68,6 +75,6 @@ AddButton(Tab2o, {
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
   end
-  }) AddButton(Tab1o, {
+  }) AddButton(Tab3o, {
     Name = "CHEST VGB",
     Callback = function()loadstring(game:HttpGet("https://raw.githubusercontent.com/VGB-VGB-VGB/-VGB-Chest-Farm--/refs/heads/main/ChestFarmByVGBTeam"))() end })
